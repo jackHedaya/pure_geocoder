c848c8428795b75df9ce806eff45dac005e26a89
Jack Hedaya <jackehedaya@gmail.com>
2018-09-05 23:37:15 -0400

    test for geocoding

--- test/pure_geocoder_test.exs
+++ test/pure_geocoder_test.exs
@@ -1,8 +1,8 @@
 defmodule PureGeocoderTest do
   use ExUnit.Case
-  doctest PureGeocoder
 
-  test "greets the world" do
-    assert PureGeocoder.hello() == :world
+  test "gets the proper coordinates for the Empire State Building" do
+    assert PureGeocoder.geocode("20 West 34th St, New York, NY 10118") ==
+        %{latitude: 40.7496458, longitude: -73.9874169}
   end
 end
