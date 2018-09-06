aa1709594f67e9160a730af4d8aafb3fc16716a5
Jack Hedaya <jackehedaya@gmail.com>
2018-09-05 22:59:52 -0400

    currently gets the location code for an address in the OSM database

--- lib/pure_geocoder.ex
+++ lib/pure_geocoder.ex
@@ -1,18 +1,60 @@
 defmodule PureGeocoder do
   @moduledoc """
-  Documentation for PureGeocoder.
+  A module that geocodes addresses and reverse geocodes coordinates.
   """
 
   @doc """
-  Hello world.
+  Geocodes an address.
+
+  Returns `{:ok, %{longitude, latitude}}` or `{:error, reason}`
 
   ## Examples
 
-      iex> PureGeocoder.hello
+      iex> PureGeocoder.geocode("20 West 34th St, New York, NY 10118")
       :world
 
   """
-  def hello do
-    :world
+  def geocode(string) do
+    remove_spaces = string |> String.replace(" ", "+")
+    body = HTTPoison.get("https://nominatim.openstreetmap.org/search.php?q=" <> remove_spaces)
+
+    case body do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        get_location_code(body)
+      {:ok, %HTTPoison.Response{status_code: _}} ->
+        {:error, "Error while reaching OpenStreetMap!"}
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, "HTTPoison error: \"" <> reason <> "\""}
+    end
+  end
+
+  defp get_location_code(body) do
+    parsed = Floki.parse(body)
+
+    data = Floki.find(parsed, ".details")
+
+    if length(data) > 0 do
+      {_, props, _} = List.first(data)
+
+      {_, val} = find_prop_with_key(props, "href")
+
+      Regex.run(~r/^details\.php\?place_id=(\d*)$/, val)
+      |> tl()
+      |> hd()
+    else
+      nil
+    end
+  end
+
+  defp find_prop_with_key(props, key) do
+    Enum.find(props, fn x ->
+      case x do
+        {^key, _} -> true
+        _ -> false
+      end
+    end)
+  end
+  def reverse_geocode(lat, lng) do
+
   end
 end
