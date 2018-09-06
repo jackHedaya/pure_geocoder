9dd3eaf9b1a0ff632fee5913d6b2f2ad16068a09
Jack Hedaya <jackehedaya@gmail.com>
2018-09-05 22:59:18 -0400

    add package description and metadata

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule PureGeocoder.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -15,6 +17,21 @@ defmodule PureGeocoder.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp description do
+    """
+    Library for geocoding addresses and reverse geocoding coordinates. Uses OpenStreetMap.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Jack Hedaya"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/jackHedaya/pure_geocoder"}
     ]
   end
 
