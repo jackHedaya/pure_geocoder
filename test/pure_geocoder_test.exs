475fb2062ca80b5ceef1949c11bc1f7673715b85
Jack Hedaya <jackehedaya@gmail.com>
2018-09-09 18:16:31 -0400

    corrected tests

--- test/pure_geocoder_test.exs
+++ test/pure_geocoder_test.exs
@@ -1,8 +1,13 @@
 defmodule PureGeocoderTest do
   use ExUnit.Case
 
-  test "gets the proper coordinates for the Empire State Building" do
-    assert PureGeocoder.geocode("20 West 34th St, New York, NY 10118") ==
-             {:ok, %{lat: 40.7486538125, lng: -73.9853043125}}
+  test "geocodes the Empire State Building properly" do
+    assert PureGeocoder.geocode("350 5th Ave, New York, NY 10118") ==
+             {:ok, %{lat: 40.7483271, lng: -73.9856549}}
+  end
+
+  test "reverse geocodes the Empire State Building properly" do
+    assert PureGeocoder.reverse_geocode(%{lat: 40.7483271, lng: -73.9856549}) ==
+      {:ok, "350 5th Avenue, NYC, New York, 10018, USA"}
   end
 end
