98c36f0254b1058ff6c5ade9f16d8449c79fd46e
Jack Hedaya <jackehedaya@gmail.com>
2018-09-09 17:42:59 -0400

    added :ok to geocode result

--- test/pure_geocoder_test.exs
+++ test/pure_geocoder_test.exs
@@ -3,6 +3,6 @@ defmodule PureGeocoderTest do
 
   test "gets the proper coordinates for the Empire State Building" do
     assert PureGeocoder.geocode("20 West 34th St, New York, NY 10118") ==
-        %{latitude: 40.7496458, longitude: -73.9874169}
+             {:ok, %{lat: 40.7486538125, lng: -73.9853043125}}
   end
 end
